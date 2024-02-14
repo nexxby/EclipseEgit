84229a54ac5aa30d131007633b1919ab6f0ac871
ZhiLi <ZhiLi@CSMD3PC00087-CA>
2024-02-14 16:50:27 +0100

    tercer commit master

--- pruebaEgit/src/pruebaEgit/pruebaEgit.java
+++ pruebaEgit/src/pruebaEgit/pruebaEgit.java
@@ -5,7 +5,7 @@ public class pruebaEgit {
 	public static void main(String[] args) {
 		System.out.println("Prueba Egit");
 		System.out.println("segundo commit master");
-
+		System.out.println("tercer commit master");
 	}
 
 }
